bf1bb5e6ab162333ff3981bb6135873978541075
Zer0Poke <joselito.jo@hotmail.com>
2022-09-19 17:19:42 -0300

    Other Components

---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                        <h1>DSMeta</h1>
+                        <p>Desenvolvido por 
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> Zero</a>
+                        </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
